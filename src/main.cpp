f0be71107ef8240cf3564268aefdea89a9ea6250
Raahim Fareed <raahim1965@hotmail.com>
2021-01-04 02:56:56 +0500

    Admin section

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,9 @@
 #include <limits>
 #include <string>
 #include <algorithm>
+#include <cctype>
+#include <stdlib.h>
+#include <ctime>
 
 /**
  * Namespaces
@@ -105,6 +108,22 @@ void Patient(const string &username, string &menu_location, string &submenu_loca
 void GetUserMessages(const string &username);
 void GetDoctorsAvailable();
 void GetUserInformation(const string &username);
+void ManagePatientsMenu(const string &username, string &menu_location, string &submenu_location);
+void ManagePatients(const string &username, string &menu_location, string &submenu_location);
+void GetTotalPendingAppointments();
+void MessageUser();
+void GetAllPatients();
+void Admin(string &menu_location, string &submenu_location);
+void AdminGetUserMessages();
+void ClearPatientsDb();
+void GetAllDoctors();
+void ClearDoctorsDb();
+void AdminGetAppointments();
+void GetAllAdmins();
+void FormatProgram();
+void GetLog();
+char *GetCurrentTime();
+void CreateLog(const string &user, char *time, char type);
 
 // Structs
 struct PatientAppointment
@@ -257,12 +276,12 @@ int main()
 			{
 				cout << Error(8) + '\n' // User needs to login
 					 << "Press [Enter/Return] Key to return to main menu\n";
+				cin.get();
 			}
-			else
+			else if (g_user_precedence == 4)
 			{
-				
+				ManagePatients(g_username, menu_location, submenu_location);
 			}
-			cin.get();
 			break;
 		case 'D':
 			if (g_user_precedence != 4)
@@ -279,44 +298,7 @@ int main()
 		case 'M':
 			if (g_user_precedence >= 8 && g_logged_in)
 			{
-				bool loop;
-				do
-				{
-					AdminMainMenu(g_username, menu_location, submenu_location);
-					cin >> menu_option;
-					cin.ignore(numeric_limits<streamsize>::max(), '\n');
-					loop = false;
-					switch (toupper(menu_option[0]))
-					{
-					case 'P':
-						AdminSubmenu(g_username, menu_location, submenu_location, "p");
-						cin >> menu_option;
-						cin.ignore(numeric_limits<streamsize>::max(), '\n');
-						break;
-					case 'D':
-						AdminSubmenu(g_username, menu_location, submenu_location, "d");
-						cin >> menu_option;
-						cin.ignore(numeric_limits<streamsize>::max(), '\n');
-						break;
-					case 'M':
-						AdminSubmenu(g_username, menu_location, submenu_location, "m");
-						cin >> menu_option;
-						cin.ignore(numeric_limits<streamsize>::max(), '\n');
-						break;
-					case 'H':
-						loop = true;
-						break;
-					default:
-						if (toupper(menu_option[0]) == 'E')
-						{
-							break;
-						}
-						loop = true;
-						cout << Error(1) + '\n' // Invalid option
-							 << "Press [Enter/Return] Key to continue\n";
-						cin.get();
-					}
-				} while (loop);
+				Admin(menu_location, submenu_location);
 			}
 			else
 			{
@@ -386,20 +368,19 @@ void MainMenu(const string &username, string &menu_location, string &submenu_loc
 			 << "\u001b[1;33m[S]\u001b[0m Sign up\n";
 	}
 
-	if (g_user_precedence > 0)
+	if (g_user_precedence == 2)
 	{
 		cout << "\u001b[1;33m[A]\u001b[0m Account\n";
 	}
 
-	if (g_user_precedence > 3)
+	if (g_user_precedence > 3 && g_user_precedence < 8)
 	{
 		cout << "\u001b[1;33m[P]\u001b[0m Patients\n";
 	}
 
 	if (g_user_precedence > 7)
 	{
-		cout << "\u001b[1;33m[D]\u001b[0m Doctors\n"
-		 	 << "\u001b[1;33m[M]\u001b[0m Management\n";
+		cout << "\u001b[1;33m[M]\u001b[0m Management\n";
 	}
 
 	cout << "\u001b[1;33m[H]\u001b[0m Help\n"
@@ -477,7 +458,7 @@ void AdminMainMenu(const string &username, string &menu_location, string &submen
 void AdminSubmenu(const string &username, string &menu_location, string &submenu_location, string submenu_type)
 {
 	system("cls");
-	menu_location = "admin-main-menu";
+	menu_location = "admin-menu";
 	int menu_level = submenu_type.length();
 
 	Header();
@@ -493,18 +474,20 @@ void AdminSubmenu(const string &username, string &menu_location, string &submenu
 		{
 		case 'P':
 			submenu_location = "patients";
-			cout << "\u001b[1;33m[A]\u001b[0m Show all Patients\n"
+			cout << "\u001b[1;33m[P]\u001b[0m Show all Patients\n"
+				 << "\u001b[1;33m[M]\u001b[0m Show all Messages\n"
+				 << "\u001b[1;33m[A]\u001b[0m Show all Appointments\n"
 				 << "\u001b[1;33m[R]\u001b[0m Delete Patients Database \u001b[1;31m[WARNING: IRREVERSIBLE]\u001b[0m\n";
 			break;
 		case 'D':
 			submenu_location = "doctors";
-
 			cout << "\u001b[1;33m[A]\u001b[0m Show all Doctors\n"
 				 << "\u001b[1;33m[R]\u001b[0m Delete Doctors Database \u001b[1;31m[WARNING: IRREVERSIBLE]\u001b[0m\n";
 			break;
 		case 'M':
 			submenu_location = "admins";
 			cout << "\u001b[1;33m[A]\u001b[0m Show all Admins\n"
+				 << "\u001b[1;33m[L]\u001b[0m Show Log\n"
 				 << "\u001b[1;33m[X]\u001b[0m Format Program \u001b[1;31m[WARNING: ALL DATA WILL BE DELETED!!!]\u001b[0m\n";
 
 			break;
@@ -519,7 +502,6 @@ void AdminSubmenu(const string &username, string &menu_location, string &submenu
 		 << "----------------------------------\n";
 
 	InputPlaceholder(username, menu_location, submenu_location);
-	system("pause");
 }
 
 void Signup(string type)
@@ -632,6 +614,7 @@ bool Login(string type)
 			cout << "\u001b[1;32mLogged In!\u001b[0m\n"
 				 << "Press [Enter/Return] Key to return to main menu\n";
 			PrecedenceSet(tolower(type[0]));
+			CreateLog(username, GetCurrentTime(), type[0]);
 			// cin.ignore(numeric_limits<streamsize>::max(), '\n');
 			cin.get();
 			return true;
@@ -650,6 +633,7 @@ bool Login(string type)
 				cout << "\u001b[1;32mLogged In!\u001b[0m\n"
 					 << "Press [Enter/Return] Key to return to main menu\n";
 				PrecedenceSet(tolower(type[0]));
+				CreateLog(username, GetCurrentTime(), type[0]);
 				// cin.ignore(numeric_limits<streamsize>::max(), '\n');
 				cin.get();
 				return true;
@@ -694,6 +678,7 @@ string Error(int error_code)
 		"You need to be logged in!",						  // 8
 		"Restricted Access",								  // 9
 		"You need to be a doctor to access this",			  // 10
+		"Your message cannot be empty!",					  // 11
 	};
 
 	return "\u001b[1;31m" + errors[error_code] + "\u001b[0m";
@@ -800,7 +785,7 @@ void GetUserMessages(const string &username)
 	bool no_messages = true;
 	cout << "Doctor name\t\tMessage\n";
 	cout << "----------------------------------\n";
-	for (int i = 0; i < c_total_patient_message_limit; i++)
+	for (int i = 0; i < g_total_messages; i++)
 	{
 		if (PatientMessages[i].patient == username)
 		{
@@ -868,12 +853,11 @@ void ManagePatientsMenu(const string &username, string &menu_location, string &s
 
 	Header();
 	cout << "----------------------------------\n"
-		 << "\u001b[1;36mAccount\u001b[0m\n"
+		 << "\u001b[1;36mPatients\u001b[0m\n"
 		 << "----------------------------------\n"
-		 << "\u001b[1;33m[R]\u001b[0m Request Appointment\n"
-		 << "\u001b[1;33m[D]\u001b[0m Doctors Information\n"
-		 << "\u001b[1;33m[U]\u001b[0m User Information\n"
-		 << "\u001b[1;33m[M]\u001b[0m Messages\n"
+		 << "\u001b[1;33m[A]\u001b[0m Pending Appointments\n"
+		 << "\u001b[1;33m[M]\u001b[0m Send Message to Patient\n"
+		 << "\u001b[1;33m[P]\u001b[0m Patient Info\n"
 		 << "\033[1;33m[H]\033[0m Help\n"
 		 << "\033[1;33m[E]\033[0m Previous Menu\n"
 		 << "----------------------------------\n";
@@ -887,43 +871,23 @@ void ManagePatients(const string &username, string &menu_location, string &subme
 	string patient_menu;
 	do
 	{
-		PatientMenu(username, menu_location, submenu_location);
+		ManagePatientsMenu(username, menu_location, submenu_location);
 		cin >> patient_menu;
 		cin.ignore(numeric_limits<streamsize>::max(), '\n');
 		switch (toupper(patient_menu[0]))
 		{
-		case 'R':
-			if (g_total_appointments <= 1000)
-			{
-				Appointments[g_total_appointments].username = username;
-				cout << "Please enter your condition/query: ";
-				getline(cin, Appointments[g_total_appointments].query);
-				g_total_appointments++;
-				cout << "Check your messages occasionally for a reply from the doctor\n"
-					 << "Press [Enter/Return] Key to return back to menu\n";
-				cin.get();
-			}
-			else
-			{
-				cout << Error(0) + '\n' // Error occurred
-					 << "Press [Enter/Return] Key to return to main menu\n";
-				cin.get();
-			}
-			break;
-		case 'D':
-			GetDoctorsAvailable();
+		case 'A':
+			GetTotalPendingAppointments();
 			cout << "Press [Enter/Return] Key to continue\n";
 			cin.get();
 			break;
-		case 'U':
-			GetUserInformation(username);
+		case 'P':
+			GetAllPatients();
 			cout << "Press [Enter/Return] Key to continue\n";
 			cin.get();
 			break;
 		case 'M':
-			GetUserMessages(username);
-			cout << "Press [Enter/Return] Key to continue\n";
-			cin.get();
+			MessageUser();
 			break;
 		case 'E':
 			patient_loop = false;
@@ -932,4 +896,397 @@ void ManagePatients(const string &username, string &menu_location, string &subme
 			break;
 		}
 	} while (patient_loop);
+}
+
+void GetTotalPendingAppointments()
+{
+	cout << "----------------------------------\n"
+		 << "Id\tPatient\tQuery\n"
+		 << "----------------------------------\n";
+
+	for (int i = 0; i < g_total_appointments; i++)
+	{
+		if (Appointments[i].username == "-")
+			continue;
+		cout << to_string(i) + '\t' + Appointments[i].username + '\t' + Appointments[i].query + '\n';
+	}
+}
+
+void MessageUser()
+{
+	string message;
+	string patient;
+	string user_data;
+	string delimiter = ";";
+	string path = "p_accounts.db";
+	string patient_check;
+	bool patient_exists = false;
+	bool first_try = true;
+
+	do
+	{
+		if (!first_try)
+		{
+			cout << "Press [0] to exit to menu\n";
+		}
+		cout << "Please enter your patient's username: ";
+		getline(cin, patient);
+		cout << patient << '\n';
+		if (patient == "0")
+			return;
+		ifstream users_file_i(path);
+		while (getline(users_file_i, user_data))
+		{
+			patient_check = user_data.substr(0, user_data.find(delimiter));
+
+			if (patient == patient_check)
+			{
+				patient_exists = true;
+				break;
+			}
+		}
+		users_file_i.close();
+
+		if (!patient_exists)
+		{
+			first_try = false;
+			cout << Error(3) + '\n'
+				 << "Press [Enter/Return] Key to return to retry\n";
+			cin.get();
+		}
+	} while (!patient_exists);
+	first_try = true;
+	do
+	{
+		if (!first_try)
+		{
+			cout << "Press [0] to exit to menu\n";
+		}
+		cout << "Please enter your message: ";
+		getline(cin, message);
+		if (message == "0")
+			return;
+		if (message.empty())
+		{
+			first_try = false;
+			cout << Error(11) + '\n'
+				 << "Press [Enter/Return] Key to return to retry\n";
+
+			cin.get();
+		}
+	} while (message.empty());
+
+	PatientMessages[g_total_messages].doctor = g_username;
+	PatientMessages[g_total_messages].patient = patient;
+	PatientMessages[g_total_messages].message = message;
+	g_total_messages++;
+
+	cout << "Message sent to " + patient + '\n'
+		 << "Press [Enter/Return] Key to return to menu\n";
+	cin.get();
+}
+
+void GetAllPatients()
+{
+	string user_data;
+	string delimiter = ";";
+	string patient_username;
+	string path = "p_accounts.db";
+	cout << "----------------------------------\n"
+		 << "Patient Name\n"
+		 << "----------------------------------\n";
+	ifstream users_file_i(path);
+	while (getline(users_file_i, user_data))
+	{
+		patient_username = user_data.substr(0, user_data.find(delimiter));
+		cout << patient_username + '\n';
+	}
+	users_file_i.close();
+}
+
+void GetAllDoctors()
+{
+	string user_data;
+	string delimiter = ";";
+	string doctor_username;
+	string path = "d_accounts.db";
+	cout << "----------------------------------\n"
+		 << "Doctor Name\n"
+		 << "----------------------------------\n";
+	ifstream users_file_i(path);
+	while (getline(users_file_i, user_data))
+	{
+		doctor_username = user_data.substr(0, user_data.find(delimiter));
+		cout << doctor_username + '\n';
+	}
+	users_file_i.close();
+}
+
+void GetAllAdmins()
+{
+	string user_data;
+	string delimiter = ";";
+	string admin_username;
+	string path = "m_accounts.db";
+	cout << "----------------------------------\n"
+		 << "Admin Name\n"
+		 << "----------------------------------\n";
+	ifstream users_file_i(path);
+	while (getline(users_file_i, user_data))
+	{
+		admin_username = user_data.substr(0, user_data.find(delimiter));
+		cout << admin_username + '\n';
+	}
+	users_file_i.close();
+}
+
+void Admin(string &menu_location, string &submenu_location)
+{
+	menu_location = "admin-menu";
+	bool loop;
+	char menu_option;
+	do
+	{
+		AdminMainMenu(g_username, menu_location, submenu_location);
+		cin >> menu_option;
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		loop = false;
+		switch (toupper(menu_option))
+		{
+		case 'P':
+			AdminSubmenu(g_username, menu_location, submenu_location, "p");
+			cin >> menu_option;
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			switch (toupper(menu_option))
+			{
+				case 'P':
+					GetAllPatients();
+					cout << "Please press [Enter/Return] key to return to menu";
+					cin.get();
+					break;
+				case 'R':
+					ClearPatientsDb();
+					break;
+				case 'M':
+					AdminGetUserMessages();
+					break;
+				case 'A':
+					AdminGetAppointments();
+					break;
+				default:
+					cout << Error(1) + '\n'
+						 << "Please press [Enter/Return] key to return to menu";
+			}
+			break;
+		case 'D':
+			AdminSubmenu(g_username, menu_location, submenu_location, "d");
+			cin >> menu_option;
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			switch (toupper(menu_option))
+			{
+				case 'A':
+					GetAllDoctors();
+					cout << "Please press [Enter/Return] key to return to menu";
+					cin.get();
+					break;
+				case 'R':
+					ClearDoctorsDb();
+					break;
+			}
+			break;
+		case 'M':
+			AdminSubmenu(g_username, menu_location, submenu_location, "m");
+			cin >> menu_option;
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			switch (toupper(menu_option))
+			{
+				case 'A':
+					GetAllAdmins();
+					cout << "Please press [Enter/Return] key to return to menu";
+					cin.get();
+					break;
+				case 'L':
+					GetLog();
+					cout << "Please press [Enter/Return] key to return to menu";
+					cin.get();
+					break;
+				case 'X':
+					FormatProgram();
+					break;
+			}
+			break;
+		case 'H':
+			loop = true;
+			break;
+		case 'E':
+			loop = false;
+			break;
+		default:
+			loop = true;
+			cout << Error(1) + '\n' // Invalid option
+				 << "Press [Enter/Return] Key to continue\n";
+			cin.get();
+		}
+	} while (loop);
+}
+
+void ClearPatientsDb()
+{
+	char confirmation;
+	cout << "Are you sure you want to delete all user data? (Y/N) ";
+	cin >> confirmation;
+	cin.ignore();
+	if (toupper(confirmation) == 'Y')
+	{
+		ofstream file;
+		file.open("p_accounts.db", ofstream::out | ofstream::trunc);
+		file.close();
+		cout << "All patient data has been deleted!\n";
+	}
+	else
+	{
+		cout << "Aborted operation\n";
+		
+	}
+	
+
+	cout << "Press [Enter/Return] Key to return to main menu\n";
+	cin.get();
+}
+
+void ClearDoctorsDb()
+{
+	char confirmation;
+	cout << "Are you sure you want to delete all doctor data? (Y/N) ";
+	cin >> confirmation;
+	cin.ignore();
+	if (toupper(confirmation) == 'Y')
+	{
+		ofstream file;
+		file.open("d_accounts.db", ofstream::out | ofstream::trunc);
+		file.close();
+		cout << "All doctor data has been deleted!\n";
+	}
+	else
+	{
+		cout << "Aborted operation\n";
+	}
+
+	cout << "Press [Enter/Return] Key to return to main menu\n";
+	cin.get();
+}
+
+void AdminGetUserMessages()
+{
+	cout << "----------------------------------\n"
+		 << "Id\t\tDoctor\t\tPatient\t\tMessage\n"
+		 << "----------------------------------\n";
+	for (int i = 0; i < g_total_messages; i++)
+	{
+		cout << i + 1 << '\t' + PatientMessages[i].doctor + '\t' + PatientMessages[i].patient + '\t' + PatientMessages[i].message + '\n';
+	}
+	cout << "----------------------------------\n";
+	cout << "Press [Enter/Return] Key to return to menu\n";
+	cin.get();
+}
+
+void AdminGetAppointments()
+{
+	cout << "----------------------------------\n"
+		 << "Id\t\tPatient\t\tQuery\n"
+		 << "----------------------------------\n";
+	for (int i = 0; i < g_total_appointments; i++)
+	{
+		cout << i + 1 << '\t' + Appointments[i].username + '\t' + Appointments[i].query + '\n';
+	}
+	cout << "----------------------------------\n";
+	cout << "Press [Enter/Return] Key to return to menu\n";
+	cin.get();
+}
+
+void FormatProgram()
+{
+	char confirmation;
+	cout << "Are you sure you want to format the program? It is irreversible. All data and all accounts will be deleted forever! (Y/N) ";
+	cin >> confirmation;
+	cin.ignore();
+	if (toupper(confirmation) == 'Y')
+	{
+		ofstream file;
+		file.open("p_accounts.db", ofstream::out | ofstream::trunc);
+		file.close();
+		file.open("d_accounts.db", ofstream::out | ofstream::trunc);
+		file.close();
+		file.open("m_accounts.db", ofstream::out | ofstream::trunc);
+		file.close();
+		file.open("log.db", ofstream::out | ofstream::trunc);
+		file.close();
+		cout << "Program has been formatted!\n"
+			 << "Program needs to be restarted to complete the process\n"
+			 << "Press [Enter/Return] key to continue and close the program.\n";
+		cin.get();
+		exit(EXIT_SUCCESS);
+	}
+	else
+	{
+		cout << "Aborted operation\n"
+			 << "Press [Enter/Return] Key to return to main menu\n";
+		cin.get();
+	}
+
+}
+
+void GetLog()
+{
+	string log_data;
+	string delimiter = ";";
+	string username;
+	string type;
+	string time;
+	string path = "log.db";
+	cout << "----------------------------------\n"
+		 << "Type\t\tUser\t\tTime of Login\n"
+		 << "----------------------------------\n";
+	ifstream file_i(path);
+	while (getline(file_i, log_data))
+	{
+		username = log_data.substr(0, log_data.find(delimiter));
+		time = log_data.erase(0, log_data.find(delimiter) + delimiter.length());
+		switch(toupper(username[0]))
+		{
+			case 'M':
+				type = "Admin";
+				break;
+			case 'D':
+				type = "Doctor";
+				break;
+			case 'P':
+				type = "Patient";
+				break;
+		}		
+		username = username.replace(0, 1, "");
+		cout << type + "\t\t" + username + "\t\t" + time + '\n';
+	}
+	file_i.close();
+}
+
+char *GetCurrentTime()
+{
+	// current date/time based on current system
+	// time_t is a struct used in ctime header
+   time_t now = time(0);
+   
+   // convert now to string form
+   char* dt = ctime(&now);
+
+   return dt;
+}
+
+void CreateLog(const string &user, char *time, char type)
+{
+	type = (char) toupper(type);
+	ofstream log_file("log.db", ios::out | ios::app);
+	string log_line = type + user + string(";") + time;
+	log_file << log_line;
+	log_file.close();
 }
